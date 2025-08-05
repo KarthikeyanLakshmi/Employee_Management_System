951b23bc62b5a820692f6f5f7d78e9497d234bb2
Lakshmi Narayanan <lakshminarayanan198@gmail.com>
2025-08-05 09:41:56 +0800

    app routes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { EmployeeAdd } from './components/employee-add/employee-add';
+import { EmployeeAddForm } from './components/employee-add/employee-add';
 
 export const routes: Routes = [
-  { path: '', component: EmployeeAdd, title: 'Employee Form' }
+  { path: '', component: EmployeeAddForm, title: 'Employee Form' }
 ];
